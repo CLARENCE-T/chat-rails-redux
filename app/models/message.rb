ed30733b0fe67bdd4db1615e49dc978ae286c08e
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-10-26 10:37:50 +0200

    init git repo

---
+++ app/models/message.rb
@@ -0,0 +1,22 @@
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :channel
+
+  validates :content, presence: true
+
+  # after_create :broadcast_message
+
+  def as_json(options = {})
+
+    nickname = user.nickname.nil? ? user.email.match(/[^@]+/)[0] : user.nickname
+
+    {
+      id: id,
+      author: nickname,
+      content: content,
+      created_at: created_at,
+      channel: channel.name
+    }
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_172444) do
+ActiveRecord::Schema.define(version: 2021_10_25_173240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "channel_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["channel_id"], name: "index_messages_on_channel_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +39,16 @@ ActiveRecord::Schema.define(version: 2021_10_25_172444) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "channels"
+  add_foreign_key "messages", "users"
 end
